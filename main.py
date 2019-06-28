ad4b684277cca5f13b0b0d46d2366e0d5c95fd6e
Ana Clara Kandratavicius <anakandrat@gmail.com>
2019-06-28 19:52:43 -0300

    Added comments.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import dask.dataframe as dd
 
 FILE1 = 'data/big_file1.csv'
-FILE2 = 'data/big_file2.csv'
+FILE2 = 'data/big_file1.csv'
 
 
 def main():
+    # create two dask dataframes (optimized for large datasets)
     df1 = dd.read_csv(FILE1)
     df2 = dd.read_csv(FILE2)
 
+    # merge them by doing inner join operation
     df3 = dd.merge(df1, df2, on=['name', 'age', 'email'], how='inner')
 
     print(df3.head())
